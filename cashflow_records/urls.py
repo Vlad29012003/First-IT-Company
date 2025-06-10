2fb1e4cc5b7d73e6e2bddc25f844fbe473743017
Vlad <vladosmen29@gmail.com>
2025-06-10 22:54:00 +0600

    added crud and templates cashFlow

--- cashflow_records/urls.py
+++ cashflow_records/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import CashFlowListView
+from .views import CashFlowListView, CashFlowCreateView, CashFlowUpdateView, CashFlowDeleteView
 
 urlpatterns = [
     path('', CashFlowListView.as_view(), name='cashflow_list'),
+    path('add/', CashFlowCreateView.as_view(), name='cashflow_add'),
+    path('edit/<int:pk>/', CashFlowUpdateView.as_view(), name='cashflow_edit'),
+    path('delete/<int:pk>/', CashFlowDeleteView.as_view(), name='cashflow_delete'),
 ] 
--- cashflow_records/views.py
+++ cashflow_records/views.py
@@ -1,12 +1,25 @@
-from django_filters.views import FilterView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import CashFlowRecord
-from .filters import CashFlowRecordFilter
 
-
-
-class CashFlowListView(FilterView):
+class CashFlowListView(ListView):
     model = CashFlowRecord
     template_name = 'cashflow_records/cashflow_list.html'
     context_object_name = 'records'
-    filterset_class = CashFlowRecordFilter
-    ordering = ['-created_at']
+
+class CashFlowCreateView(CreateView):
+    model = CashFlowRecord
+    fields = '__all__'
+    template_name = 'cashflow_records/cashflow_form.html'
+    success_url = reverse_lazy('cashflow_list')
+
+class CashFlowUpdateView(UpdateView):
+    model = CashFlowRecord
+    fields = '__all__'
+    template_name = 'cashflow_records/cashflow_form.html'
+    success_url = reverse_lazy('cashflow_list')
+
+class CashFlowDeleteView(DeleteView):
+    model = CashFlowRecord
+    template_name = 'cashflow_records/cashflow_confirm_delete.html'
+    success_url = reverse_lazy('cashflow_list')
